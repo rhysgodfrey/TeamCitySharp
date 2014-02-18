9356eba5933272a194f1b07c1b766febd056b4b9
Rhys Godfrey <rhys.godfrey@vouchercloud.com>
2014-02-18 22:50:11 +0000

    Adding State field to build data
    The state field shows if the build has finished or is still running.

--- src/TeamCitySharp/DomainEntities/Build.cs
+++ src/TeamCitySharp/DomainEntities/Build.cs
@@ -7,6 +7,7 @@ namespace TeamCitySharp.DomainEntities
         public string Id { get; set; }
         public string Number { get; set; }
         public string Status { get; set; }
+        public string State { get; set; }
         public string BuildTypeId { get; set; }
         public string Href { get; set; }
         public string WebUrl { get; set; }
